9bc422077bea1034567a7cf0876271877af552fb
4010g13 <4010g13@acerola.ece.uprm.edu>
2018-08-29 18:25:25 -0400

    It adds the seniority level feature

--- src/Employee.java
+++ src/Employee.java
@@ -81,9 +81,18 @@ public class Employee {
 	 */
 	public int seniorityLevel() {
 		
-		//Add Code Here [You have to use If/Else to get graded]
-		
-		return 0;// Temporal Return
+		if(yearService >= 10){
+			return 3;
+		}
+		else if (yearService >= 5){
+			return 2;
+		}
+		else if (yearService >= 1){
+			return 1;
+		}
+		else{
+			return 0;
+		}
 		
 	}
 	
